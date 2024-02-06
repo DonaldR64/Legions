1d52a9f455ad31cbaae4eca3395ad8242c6d1edf
DonaldR64 <don.rhodes@icloud.com>
2024-02-06 17:08:16 -0500

    create blast added

--- LI.js
+++ LI.js
@@ -3393,6 +3393,62 @@ log(model.name)
     
     }
 
+const CreateBlast = (msg) => {
+    let Tag = msg.content.split(";");
+    let shooterID = Tag[1];
+    let weaponNum = parseInt(Tag[2]);
+    let shooter = ModelArray[shooterID];
+    let weapon = shooter.weaponArray[weaponNum];
+    SetupCard(shooter.name,"Blast",shooter.faction);
+    if (shooter.weaponsFired.includes(weaponNum)) {
+        outputCard.body.push("Already Fired this Weapon this Turn");
+        PrintCard();
+        return;
+    }
+    //find radius, create token/model and add abilities - Check LOS and Fire!
+    let traits = weapon.traits.split(",");
+    let radius;
+    _.each(traits,trait => {
+        if (trait.includes("Blast")) {
+            radius = trait;
+        }
+    });
+    radius = radius.replace(/\D/g,'') - 1;
+    let img = "https://s3.amazonaws.com/files.d20.io/images/105823565/P035DS5yk74ij8TxLPU8BQ/thumb.png?15826799915";
+    img = getCleanImgSrc(img);
+    let represents = "-NAZtEQYwkNjQqZmyabb";
+    let abilArray = findObjs({  _type: "ability", _characterid: represents});
+    //clear old abilities
+    for(let a=0;a<abilArray.length;a++) {
+        abilArray[a].remove();
+    } 
+    let abilityAction = "!CheckBlastLOS;@{selected|token_id}";
+    AddAbility("Check LOS/Range",abilityAction,represents);
+    abilityAction = "!Shooting;Blast;@{selected|token_id};@{target|token_id};No;" + weaponNum;
+    AddAbility("Fire " + weapon.name,abilityAction,represents);
+    let newToken = createObj("graphic", {   
+        left: shooter.location.x,
+        top: shooter.location.y,
+        width: 70, 
+        height: 70,  
+        represents: represents,
+        name: "Blast Target",
+        pageid: Campaign().get("playerpageid"),
+        imgsrc: img,
+        layer: "objects",
+        aura1_color: "#FF0000",
+        aura1_radius: radius,
+    });
+    toFront(newToken);
+    let model = new Model(newToken.id,0,0);
+    outputCard.body.push("Move into Place");
+    outputCard.body.push("Select Fire when Done");
+    PrintCard();
+}
+
+
+
+
     
 
 //amend below for multiple templates
@@ -3681,6 +3737,11 @@ log(hexMap[target.hexLabel])
             case '!Shooting':
                 Shooting(msg);
                 break;
+            case '!CreateBlast':
+                CreateBlast(msg);
+                break;
+
+
         }
     };
     const registerEventHandlers = () => {
